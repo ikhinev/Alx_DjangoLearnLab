f840db7a8400abbf460a5ff3f6c75271f4676da2
ikhinev <ikhinevictor1234@gmail.com>
2025-09-08 21:11:38 +0100

    Add custom permissions to Book and enforce in book_list view

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -3,11 +3,12 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    published_date = models.DateField(null=True, blank=True)
 
     class Meta:
         permissions = [
+            ("can_view", "Can view book"),
             ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
             ("can_delete", "Can delete book"),
         ]
 
--- advanced_features_and_security/bookshelf/views.py
+++ advanced_features_and_security/bookshelf/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 
-# Protect the book list view with custom permissions
-@permission_required("bookshelf.view_book", raise_exception=True)
+@permission_required("bookshelf.can_view", raise_exception=True)
 def book_list(request):
-    books = Book.objects.all()  # required: checker looks for "books"
+    books = Book.objects.all()
     return render(request, "bookshelf/book_list.html", {"books": books})
