ad2173fa32ac9de012bbb3f7fb9b4d70fa95e774
ikhinev <ikhinevictor1234@gmail.com>
2025-09-08 03:23:15 +0100

    Setup CustomUser model, admin, and Book model in bookshelf app

--- advanced_features_and_security/accounts/admin.py
+++ advanced_features_and_security/accounts/admin.py
@@ -2,23 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active")
-    fieldsets = (
-        (None, {"fields": ("username", "email", "password")}),
-        ("Personal Info", {"fields": ("first_name", "last_name", "date_of_birth", "profile_photo")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
-        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("username", "email", "password1", "password2", "is_staff", "is_active")}
-        ),
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    search_fields = ("username", "email")
-    ordering = ("username",)
-
-admin.site.register(CustomUser, CustomUserAdmin)
