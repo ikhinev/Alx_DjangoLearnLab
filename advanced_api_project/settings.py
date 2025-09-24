e6ea4fa41c58b259bd783e705b3de37da6bed00a
ikhinev <ikhinevictor1234@gmail.com>
2025-09-24 01:53:39 +0100

    Complete: Django project with custom serializers and nested models

--- advanced_api_project/settings.py
+++ advanced_api_project/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+<<<<<<< HEAD
 SECRET_KEY = 'django-insecure-dodc9nqic*svrr)dkxhjzechkjc$tyn+-gbn8kine&#^$!)9$9'
+=======
+SECRET_KEY = 'django-insecure-196^hs3m%4ryq@shs$fh-o9xvnd_*m@jtcfaq3n$ury1%1-2b_'
+>>>>>>> 822d3a6 (Complete: Django project with custom serializers and nested models)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
